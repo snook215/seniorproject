5cf239094bc3562cca637714ea28416c0f16466e
snook215 <snookeros215@gmail.com>
2020-02-19 00:00:30 +0700

    nook test

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
 }
 </script>
 
+
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { domain, clientId } from "../auth_config.json";
 // Import the plugin here
 import { Auth0Plugin } from "./auth";
 
+//testnook
 
 // Install the authentication plugin here
 Vue.use(Auth0Plugin, {
