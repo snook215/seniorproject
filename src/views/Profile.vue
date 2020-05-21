8c9b900dd0824d0341f64251397a0ff5f1290d09
snook215 <snookeros215@gmail.com>
2020-05-21 12:35:16 +0700

    init commit

---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="collectdata2">
+        <section class="hero">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+            <p class="title">Profile</p>
+            </div>
+        </div>
+        </section>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero-body {
+         background-color: rgb(204, 99, 204);
+    } 
+
+
+</style>
