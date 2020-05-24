05d68e365ff0ba51493400fb1ec511e82cd8b260
styley997 <styley997@gmail.com>
2020-05-24 20:49:11 +0700

    Done fillter student

--- src/views/Studentdata.vue
+++ src/views/Studentdata.vue
@@ -7,55 +7,75 @@
           <p class="title has-text-white">Student Data</p>
         </div>
       </div>
+    <br />
+    <br />
     </section>
-
-        <div class="has-text-centered">
-          <download-csv :data   = "users"> Download Data </download-csv>
+    <label class="label has-text-centered-desktop">Select</label>
+        <div class="field has-addons has-addons-centered">
+          <p class="control">
+            <span class="select">
+              <select v-model="fillter">
+                <option value="0">All</option>
+                <option value="1">studentid</option>
+                <option value="3">Age</option>
+                <option value="4">schoolid</option>
+              </select>
+            </span>
+          </p>
+          <p class="control">
+            <input class="input" type="text" v-model="keydata" placeholder="keyword">
+          </p>
+          <p class="control">
+            <a class="button is-success" @click="retrieveUsersfillter()">
+              Done
+            </a>
+          </p>
         </div>
-        
-        <br>
-        <br>
-        <br>
-        
-        <div class="table">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Student ID</th>
-                        <th>Name</th>
-                        <th>School</th>
-                        <th>Grade</th>
-                        <th>Class</th>
-                        <th>Age</th>
-                        <th>Gender</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(item , index) in users" :key="item.id">
-                    <th>{{index+1}}</th>
-                    <th>{{item.studentid}}</th>
-                    <td>{{item.name}}</td>
-                    <td>{{ "KMUTT"}}</td>
-                    <td>{{item.grade}}</td>
-                    <td>{{item.class}}</td>
-                    <td>{{item.age}}</td>
-                    <td>{{item.gender}}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="has-text-centered">
+      <download-csv :data="users">Download Data</download-csv>
+    </div>
 
-        <br/>
-        <br/>
-        <br>
+    <br />
+    <br />
+    <br />
 
-        <div class="field is-centered">
-                    <p class="control has-text-centered">
-                        <router-link to="/editstudent" class="button is-info">Edit</router-link>
-                    </p> 
-        </div>
+    <div class="table">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Student ID</th>
+            <th>Name</th>
+            <th>School</th>
+            <th>Grade</th>
+            <th>Class</th>
+            <th>Age</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item , index) in users" :key="item.id">
+            <th>{{index+1}}</th>
+            <th>{{item.studentid}}</th>
+            <td>{{item.name}}</td>
+            <td>{{ "KMUTT"}}</td>
+            <td>{{item.grade}}</td>
+            <td>{{item.class}}</td>
+            <td>{{item.age}}</td>
+            <td>{{item.gender}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
+    <br />
+    <br />
+    <br />
+
+    <div class="field is-centered">
+      <p class="control has-text-centered">
+        <router-link to="/editstudent" class="button is-info">Edit</router-link>
+      </p>
     </div>
   </div>
 </template>
@@ -112,15 +132,15 @@ export default {
           .catch(e => {
             console.log(e);
           });
-      // } else if (this.fillter == 2) {
-      //   DataService.getStudentByGender(this.keydata)
-      //     .then(response => {
-      //       this.users = response.data;
-      //       console.log(response.data);
-      //     })
-      //     .catch(e => {
-      //       console.log(e);
-      //     });
+        // } else if (this.fillter == 2) {
+        //   DataService.getStudentByGender(this.keydata)
+        //     .then(response => {
+        //       this.users = response.data;
+        //       console.log(response.data);
+        //     })
+        //     .catch(e => {
+        //       console.log(e);
+        //     });
       } else if (this.fillter == 3) {
         DataService.getStudentByAge(this.keydata)
           .then(response => {
@@ -130,8 +150,7 @@ export default {
           .catch(e => {
             console.log(e);
           });
-      }
-      else if (this.fillter == 4) {
+      } else if (this.fillter == 4) {
         DataService.getStudentBySchoolId(this.keydata)
           .then(response => {
             this.users = response.data;
@@ -141,7 +160,7 @@ export default {
             console.log(e);
           });
       }
-    },
+    }
   }
 };
 </script>
